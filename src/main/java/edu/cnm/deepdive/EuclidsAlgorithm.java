11cfd9de26896b81ea9a567f3c4ef06f1d16a207
darbach <danielarbach@gmail.com>
2021-02-11 17:04:55 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/EuclidsAlgorithm.java
@@ -0,0 +1,50 @@
+package edu.cnm.deepdive;
+
+/**
+ * Use Euclid's Algorithm to find the Greatest Common Divisor.
+ */
+public class EuclidsAlgorithm {
+
+
+
+
+
+
+  /**
+   * Find the Greatest Common Divisor for int's a and b. Must be a positive counting integer. If
+   * either number is not a natural counting integer (1 or greater) returns -1.
+   * @param a first int
+   * @param b second int
+   * @return Greatest common divisor for a and b
+   */
+  public int getGCD(int a, int b) {
+    //  1. Given two numbers, a and b, where:
+    //     * a is a natural counting number
+    //     * b is a natural counting number
+    if ((a > 0) || (b > 0)) {
+      //  2. Define new variables a' and b', with:
+      //     * a' = a
+      //     * b' = b
+      int aPrime = a;
+      int bPrime = b;
+
+      //  3. While (b' != 0):
+      //     * If (a' > b'), then:
+      //        * a' = (a' – b')
+      //       Otherwise:
+      //        * b' = (b' – a')
+      while (bPrime != 0) {
+        if (aPrime > bPrime) {
+          aPrime = aPrime - bPrime;
+        } else {
+          bPrime = bPrime - aPrime;
+        }
+      }
+      //  4. a' is the GCD of a and b.
+      return aPrime;
+    } else {
+      return -1;
+    }
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/EuclidsAlgorithmTest.java
@@ -0,0 +1,32 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EuclidsAlgorithmTest {
+
+  EuclidsAlgorithm euclid = new EuclidsAlgorithm();
+  // {a, b, GCD}
+  private int[][] testCases = {
+      {45, 210, 15},
+      {26_187, 1_533, 21},
+      {296, 137, 1},
+      {30, 150, 30},
+      {1_288, 152, 8},
+      {220, 1_323, 1},
+      {270, 192, 6}
+  };
+
+  @Test
+  void getGCD() {
+    for (int i = 0; i < testCases.length; i++) {
+      int a = testCases[i][0];  //input
+      int b = testCases[i][1]; //
+      int expected = testCases[i][2];
+      int result = euclid.getGCD(a, b);
+      System.out.printf("GCD(%d, %d) = EXPECTED: %d; RESULT: %d%n", a, b, expected, result);
+      assertEquals(expected, result);
+    }
+  }
+}
